9192e14575ab2ace9c70a00a5415f4975877d00f
Leigh <1142961+leighgordy@users.noreply.github.com>
2026-08-03 20:37:42 +0100

    Created an inline SVG of flowers in vase, positioned it at bottom of page

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,20 @@
 <template>
-  <main>
-    <router-view />
-  </main>
+  <router-view />
 </template>
 
 <style>
+html,
+body,
+#root {
+  height: 100%;
+  width: 100%;
+  margin: 0px;
+  padding: 0px;
+}
+#root {
+  position: relative;
+}
+
 .router-link-active {
   font-weight: bold;
   color: green;
