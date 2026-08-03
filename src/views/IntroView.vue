5d6baed6175975c84cf7e2a0f67c6cdcbbd64693
Leigh <1142961+leighgordy@users.noreply.github.com>
2026-08-03 11:24:17 +0100

    Created flowers and added scaled positioning

--- src/views/IntroView.vue
+++ src/views/IntroView.vue
@@ -1,4 +1,62 @@
 <template>
-  <h2>Intro View</h2>
+  <div class="container">
+    <div class="flower-frame">
+      <div class="flower-icon-right" />
+      <div class="flower-icon-left" />
+    </div>
+  </div>
   <router-link to="/site">Go to Site</router-link>
 </template>
+<style>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+  width: 100%;
+  height: 100vh;
+  background: #348c31;
+  @media (max-width: 1000px) {
+    align-items: center;
+  }
+}
+
+.flower-frame {
+  position: relative;
+  width: 70vw;
+  height: 90vw;
+
+  max-width: 700px;
+  max-height: 900px;
+  border: 1px solid palegreen;
+
+  @media (min-width: 1000px) {
+    margin-top: 13vh;
+  }
+}
+
+.flower-icon-right {
+  position: absolute;
+  width: 50%;
+  height: 50%;
+  left: 70%;
+  top: 40%;
+  transform: translate(-50%, -50%) rotate(0deg);
+  transform-origin: center;
+  background-image: url("./flower-head.svg");
+  background-repeat: no-repeat;
+  background-size: contain;
+}
+
+.flower-icon-left {
+  position: absolute;
+  width: 60%;
+  height: 60%;
+  left: 40%;
+  top: 75%;
+  transform: translate(-50%, -50%) rotate(-35deg);
+  transform-origin: center;
+  background-image: url("./flower-head.svg");
+  background-repeat: no-repeat;
+  background-size: contain;
+}
+</style>
