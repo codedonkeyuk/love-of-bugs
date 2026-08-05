2c6c318737ade54ac72a657b6792968bb4c5e422
Leigh <1142961+leighgordy@users.noreply.github.com>
2026-08-05 14:47:01 +0100

    Added a holding page

--- src/views/SiteView.vue
+++ src/views/SiteView.vue
@@ -1,1 +1,28 @@
-<template><h2>Site View</h2></template>
+<template>
+  <div class="page-container">
+    <div class="card">
+      <h1>Site Does not exist, but it is coming soon</h1>
+      <p>
+        It took 3 days to build the intro page, mainly because I chose to draw
+        all the assets. I'l be rolling this out piece by piece over the next few
+        days.
+      </p>
+      <p>
+        <strong>Coming Soon: A SASS bug based nightmare.</strong>
+      </p>
+    </div>
+  </div>
+</template>
+<style>
+.page-container {
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+  width: 100%;
+  height: 100vh;
+}
+.card {
+  padding: 15px;
+  max-width: 800px;
+}
+</style>
