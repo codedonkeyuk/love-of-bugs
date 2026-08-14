f98c4fabbb27e306c1fbd92f9263648a229e7b0e
Leigh <1142961+leighgordy@users.noreply.github.com>
2026-08-14 09:20:25 +0100

    Added reverse animation to sample tray

--- src/components/CatalogWrapper.vue
+++ src/components/CatalogWrapper.vue
@@ -1,9 +1,25 @@
-<script setup lang="tsx">
+<script setup lang="ts">
+import { ref } from "vue";
+import { onBeforeRouteLeave } from "vue-router";
 import CatalogInterface from "../components/CatalogInterface.vue";
+
+const isExiting = ref(false);
+
+onBeforeRouteLeave((_to, _from, next) => {
+  isExiting.value = true;
+
+  setTimeout(() => {
+    next();
+  }, 4000);
+});
 </script>
+
 <template>
   <div class="catalog-container">
-    <CatalogInterface class="catalog-sheet" />
+    <CatalogInterface
+      class="catalog-sheet"
+      :class="{ 'slide-on-exit': isExiting }"
+    />
     <slot />
   </div>
 </template>
@@ -19,6 +35,9 @@ import CatalogInterface from "../components/CatalogInterface.vue";
   pointer-events: none;
   animation: slideOff 4s cubic-bezier(0.77, 0, 0.175, 1) forwards;
 }
+.catalog-sheet.slide-on-exit {
+  animation: slideOn 4s cubic-bezier(0.77, 0, 0.175, 1) forwards;
+}
 
 @keyframes slideOff {
   0% {
@@ -26,6 +45,15 @@ import CatalogInterface from "../components/CatalogInterface.vue";
   }
   100% {
     transform: translateX(-100%);
+  }
+}
+
+@keyframes slideOn {
+  0% {
+    transform: translateX(-100%);
+  }
+  100% {
+    transform: translateX(0);
   }
 }
 </style>
